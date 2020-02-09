63b9c2f711195b53f7d1dd7d5ae15a9f3c5dc95c
vincent <vincentwei95@163.com>
2020-02-09 22:48:25 +0800

    二叉树的逆层序遍历添加扩展

--- src/tree/LevelOrderTraversalFromBottom.java
+++ src/tree/LevelOrderTraversalFromBottom.java
@@ -97,4 +97,66 @@ public class LevelOrderTraversalFromBottom {
         }
         return result;
     }
+
+    /**
+     * 扩展：二叉树的锯齿形层次遍历
+     *
+     * 给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）
+     *
+     * 给定二叉树 [3,9,20,null,null,15,7]
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     *
+     * 返回锯齿形层次遍历如下
+     *
+     * [
+     *   [3],
+     *   [20,9],
+     *   [15,7]
+     * ]
+     */
+    private List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean isReverse = false;
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+
+                // 如果没有反转左右节点，此次反转入队
+                if (!isReverse) {
+                    if (node.left != null) {
+                        queue.add(node.right);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.left);
+                    }
+                }
+                // 反转过左右节点，按正常入队
+                else {
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+                }
+
+                // 修改标志未
+                isReverse = !isReverse;
+            }
+            result.add(list);
+        }
+        return result;
+    }
 }
