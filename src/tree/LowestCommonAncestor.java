9bee2c2c3b9b7bb5ad357bd48a62dbf3ea28eb43
weiwenhuan <wenhuan.wei@ximalaya.com>
2021-08-06 10:09:18 +0800

    二叉搜索树最近公共祖先添加算法思路描述

--- src/tree/LowestCommonAncestor.java
+++ src/tree/LowestCommonAncestor.java
@@ -1,5 +1,8 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先：
  *
@@ -30,11 +33,79 @@ package tree;
 public class LowestCommonAncestor {
 
     public static void main(String[] args) {
-
+        LowestCommonAncestor main = new LowestCommonAncestor();
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        System.out.println(main.lowestCommonAncestor(root, new TreeNode(3), new TreeNode(5)).val);
     }
 
     /**
-     * 方式1：递归实现
+     * 方式：两次遍历
+     *
+     * 先各自遍历从根节点到达两个节点的路径
+     * 有了两个从根节点到达目标节点（p和q）的路径，这两个节点的分岔节点，就是公共祖先
+     *
+     * T:O(n)
+     * S:O(n)
+     */
+    private TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> pathP = getPath(root, p);
+        List<TreeNode> pathQ = getPath(root, q);
+        TreeNode ancestor = null;
+        for (int i = 0; i < pathP.size() && i < pathQ.size(); i++) {
+//            p = 6, q = 6, ancestor = 6
+//            p = 2, q = 2, ancestor = 2
+//            p = 4, q = 4, ancestor = 4
+//            p = 3, q = 5, break
+//            ancestor = 4
+            if (pathP.get(i) == pathQ.get(i)) {
+                ancestor = pathP.get(i);
+            } else {
+                // 不相同，上一个值就是公共祖先
+                break;
+            }
+        }
+        return ancestor;
+    }
+
+    /**
+     *          6
+     *        /   \
+     *       2     8
+     *      / \   / \
+     *     0   4 7   9
+     *        / \
+     *       3   5
+     * 假设要找的节点target是4，从根节点搜索
+     * 如果 当前节点 > target，如 6 > 4，往左边走
+     * 如果 当前节点 < target，如 6 < 9，往右边走
+     */
+    private List<TreeNode> getPath(TreeNode root, TreeNode target) {
+        List<TreeNode> path = new ArrayList<>();
+        TreeNode node = root;
+        while (node != target) {
+            path.add(node);
+            if (target.val < node.val) {
+                node = node.left;
+            } else if (target.val > node.val){
+                node = node.right;
+            } else {
+                break;
+            }
+        }
+        path.add(node);
+        return path;
+    }
+
+    /**
+     * 方式2：递归实现
      *
      * 根据二分搜索树的特性：
      * 节点N左子树上的所有节点的值都小于等于节点N的值
@@ -51,10 +122,14 @@ public class LowestCommonAncestor {
      * S:O(1)
      */
     private TreeNode lowestCommonAncestorRecursive(TreeNode root, TreeNode p, TreeNode q) {
-        int parentVal = root.val;
-        int pVal = p.val;
-        int qVal = q.val;
+        int parentVal = root.val; // 当前节点
+        int pVal = p.val; // 目标左节点
+        int qVal = q.val; // 目标右节点
 
+        // 因为是两个目标节点的公共节点，所以走过的路径在找到公共祖先节点之前应该是相同的
+        // 所以可以使用以下的判断
+        // 当前节点 > 两个目标节点，往左边走
+        // 当前节点 < 两个目标节点，往右边走
         if (pVal > parentVal && qVal > parentVal) {
             return lowestCommonAncestorRecursive(root.right, p, q);
         } else if (pVal < parentVal && qVal < parentVal) {
@@ -65,7 +140,7 @@ public class LowestCommonAncestor {
     }
 
     /**
-     * 方式2：循环实现
+     * 方式3：循环实现
      *
      * T:O(n)
      * S:O(1)
