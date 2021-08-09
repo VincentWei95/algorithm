dd6a2228e413481de197db86fb27172b23ee004e
weiwenhuan <wenhuan.wei@ximalaya.com>
2021-08-09 14:45:43 +0800

    添加二叉树的最近公共祖先算法

--- src/tree/LowestCommonAncestor.java
+++ src/tree/LowestCommonAncestor.java
@@ -43,7 +43,7 @@ public class LowestCommonAncestor {
         root.right = new TreeNode(8);
         root.right.left = new TreeNode(7);
         root.right.right = new TreeNode(9);
-        System.out.println(main.lowestCommonAncestor(root, new TreeNode(3), new TreeNode(5)).val);
+        System.out.println(main.lowestCommonAncestor2(root, new TreeNode(3), new TreeNode(5)).val);
     }
 
     /**
@@ -55,7 +55,7 @@ public class LowestCommonAncestor {
      * T:O(n)
      * S:O(n)
      */
-    private TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    private TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
         List<TreeNode> pathP = getPath(root, p);
         List<TreeNode> pathQ = getPath(root, q);
         TreeNode ancestor = null;
@@ -162,5 +162,59 @@ public class LowestCommonAncestor {
             }
         }
         return null;
+    }
+
+    /**
+     * 扩展：二叉树的最近公共祖先
+     *
+     * root是p、q的最近公共祖先，则只可能为以下情况之一：
+     * 1、p和q在root的子树中，且分列为root的异侧（即分别在左、右子树中）
+     * 2、p=root，且q在root的左或右子树中
+     * 3、q=root，且p在root的左或右子树中
+     *
+     *                   3
+     *                 /   \
+     *                5     1
+     *               / \   / \
+     *              6   2 0   8
+     *                 / \
+     *                7   4
+     * p=7，q=8，root=3
+     * 3 是7、8的公共祖先，5不是8的祖先，1不是7的祖先 -> 3是7、8的最近公共祖先
+     *
+     * 考虑通过递归对二叉树进行前序遍历，当遇到节点p或q时返回。
+     * 从底至顶回溯，当节点p、q在节点root的异侧时，节点root即为最近公共祖先，则向上返回root
+     *
+     * 递归解析：
+     * 1、终止条件：
+     * （1）当越过叶节点，则直接返回null
+     * （2）当root等于p、q，则直接返回root
+     * 2、递归步骤：
+     * （1）递归左子节点，返回值记为left
+     * （2）递归右子节点，返回值记为right
+     * 3、返回值：根据left和right，可展开为四种情况：
+     * （1）当left和right同时为空：说明root的左/右子树中都不包含p、q，返回null
+     * （2）当left和right同时不为空：说明p、q分列在root的异侧（分别在左/右子树），因此root为最近公共祖先，返回root
+     * （3）当left为空，right不为空：p、q都不在root的左子树中，直接返回right。具体可分为两种情况：
+     *      [1]p、q其中一个在root的右子树中，此时right指向p
+     *      [2]p、q两节点都在root的右子树中，此时的right指向最近公共祖先节点
+     * （4）当left不为空，right为空：与情况（3）同理
+     */
+    private TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 树为null，直接返回null
+        if (root == null) return null;
+        // 如果p和q中有等于root的，那么它们的最近公共祖先即为root（一个节点也可以是它自己的祖先）
+        if (root == p || root == q) return root;
+
+        // 遍历左子树，只要在左子树中找到了p或q，则先找到谁就返回谁
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        // 遍历右子树，只要在右子树中找到了p或q，则先找到谁就返回谁
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 如果左子树中p和q都找不到，则p和q一定在右子树中，右子树中先遍历到的那个就是最近公共祖先
+        if (left == null) return right;
+        // 如果右子树中p和q都找不到，则p和q一定在左子树中，左子树中先遍历到的那个就是最近公共祖先
+        if (right == null) return left;
+        // left和right均不为空时，说明p和q节点分别在root的两侧，root为它们的最近公共祖先
+        return root;
     }
 }
