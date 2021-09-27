9eba388423f1c28d24465a17440917d5322e10ce
weiwenhuan <wenhuan.wei@ximalaya.com>
2021-09-27 22:13:12 +0800

    无重复的最长子串添加修改算法描述

--- src/string/middle/LengthOfLongestSubstring.java
+++ src/string/middle/LengthOfLongestSubstring.java
@@ -38,50 +38,50 @@ public class LengthOfLongestSubstring {
      * [p, w, w, k, e, w]
      * s
      * e
-     * start = 0, end = 0, result = 1, map[p, 1], start = 0, end = 1
+     * start = 0, end = 0, result = 0, map[p, 0]; start = 0, end = 1
      * -----------------------------------------------------------------
      *
      * 步骤2：
      * [p, w, w, k, e, w]
      * s
      *     e
-     * start = 0, end = 1, result = 1, map[w, 2], start = 0, end = 2
+     * start = 0, end = 1, result = 0, map[w, 1]; start = 0, end = 2
      * -----------------------------------------------------------------
      *
      * 步骤3：
      * [p, w, w, k, e, w]
      * s
      *        e
-     * map中找到字符，start移动位置
+     * map中找到字符w有重复(map存有索引为1的w），start移动位置到map中重复字符的位置+1，即map中重复字符w的索引1+1=2，更新map重复字符w的索引为2
      * [p, w, w, k, e, w]
      *        s
      *        e
-     * start = 2, end = 2, result = 1, map[w, 3], start = 2, end = 3
+     * start = 0, end = 2, result = 0, map[w, 2]; start = 2, end = 3, result = end - start + 1 = 3 - 2 + 1 = 2
      * -----------------------------------------------------------------
      *
      * 步骤4：
      * [p, w, w, k, e, w]
      *        s
      *           e
-     * start = 2, end = 3, result = 2, map[k, 4], start = 2, end = 4
+     * start = 2, end = 3, result = 2, map[k, 3]; start = 2, end = 4
      * -----------------------------------------------------------------
      *
      * 步骤5：
      * [p, w, w, k, e, w]
      *        s
      *              e
-     * start = 2, end = 4, result = 3, map[e, 5], start = 2, end = 5
+     * start = 2, end = 4, result = 2, map[e, 4]; start = 2, end = 5
      * -----------------------------------------------------------------
      *
      * 步骤6：
      * [p, w, w, k, e, w]
      *        s
      *                 e
-     * map中找到字符，start移动位置
+     * map中找到字符w有重复(map存有索引为2的w），start移动位置到map中重复字符的位置+1，即map中重复字符w的索引2+1=3，更新map重复字符w的索引为5
      * [p, w, w, k, e, w]
-     *                 s
+     *           s
      *                 e
-     * start = 5, end = 5, result = 3, map[w, 6], start = 5, end = 6
+     * start = 2, end = 5, result = 2, map[w, 5]; start = 3, end = 5, result = end - start + 1 = 5 - 3 + 1 = 3
      * -----------------------------------------------------------------
      *
      * T:O(n)
@@ -93,10 +93,12 @@ public class LengthOfLongestSubstring {
         for (int start = 0, end = 0; end < s.length(); end++) {
             char c = s.charAt(end);
             if (map.containsKey(c)) {
-                start = Math.max(start, map.get(c));
+                // 因为 start 指针要指向不重复的字符，遇到字符相同时，需要跳过当前end指向的重复字符
+                // start 移动到重复字符的下一位，所以 + 1
+                start = Math.max(start, map.get(c) + 1);
             }
-            result = Math.max(result, end - start + 1);
-            map.put(c, end + 1);
+            result = Math.max(result, end - start + 1); // end和start都是索引，因为是计算长度所以要+1
+            map.put(c, end);
         }
         return result;
     }
