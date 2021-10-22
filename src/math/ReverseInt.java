75e8d1c4b3768e892b53dd5d55d8fd3c8c99b97b
weiwenhuan <wenhuan.wei@ximalaya.com>
2021-10-22 21:51:36 +0800

    整数反转算法修改

--- src/math/ReverseInt.java
+++ src/math/ReverseInt.java
@@ -49,9 +49,9 @@ public class ReverseInt {
             int pop = x % 10;
             x /= 10;
             // 超过Integer.MAX_VALUE
-            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev > Integer.MAX_VALUE / 10) return 0;
             // 小于 Integer.MIN_VALUE
-            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            if (rev < Integer.MIN_VALUE / 10) return 0;
             rev = rev * 10 + pop;
         }
         return rev;
