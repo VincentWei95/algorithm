5b50e06b82146bdad3f06dd50c325925d94d05c4
weiwenhuan <wenhuan.wei@ximalaya.com>
2021-11-25 22:27:44 +0800

    34.在排序数组中查找元素的第一个和最后一个位置 修改实现代码

--- src/halfsearch/SearchRange.java
+++ src/halfsearch/SearchRange.java
@@ -24,45 +24,10 @@ package halfsearch;
 public class SearchRange {
 
     /**
-     * 假设定义左右边距分别为leftIndex和rightIndex
-     *
-     * leftIndex就是查找第一个 nums[mid] >= target 的索引
-     * （怎么定义第一个 nums[mid[ >= target 的索引？因为数组是升序的，第一个 nums[mid] >= target 的索引肯定会在左边，二分法往左区间走的情况就是 right 指针递减时，
-     * 所以应该理解为 找到二分法搜索到最后一个 nums[mid] == target 的索引）
-     *
-     * rightIndex就是查找第一个 nums[mid] >target 的索引-1
-     * （怎么定义第一个 > target 的索引？因为数组是升序的，第一个 nums[mid] > target 的索引应该理解为 找到二分法搜索到第一个 nums[mid] == target 的索引）
-     *
-     *  [5, 7, 7, 8, 8, 10], target = 8, ans = nums.length = 6
-     * left            right
-     *
-     * 1、
-     *  [5, 7, 7, 8, 8, 10], target = 8, ans = 6
-     * left   mid      right
-     *
-     * left = 0, right = 5
-     * mid = (left + right) / 2 = 5 / 2 = 2
-     * nums[mid] = 7 < target, left = mid + 1 = 2 + 1 = 3
-     *
-     * 2、
-     *  [5, 7, 7, 8,  8,  10], target = 8, ans = 6
-     *          left mid right
-     *
-     * left = 3, right = 5
-     * mid = (left + right) / 2 = 8 / 2 = 4
-     * nums[mid] = 8 == target, right = mid - 1 = 5 - 1 = 4, ans = mid = 4
-     *
-     * 3、
-     *  [5, 7, 7, 8,   8,  10], target = 8, ans = 4
-     *          left right
-     *          mid
-     * left = 3, right = 4
-     * mid = (left + right) / 2 = 7 / 2 = 3
-     * nums[mid] = 8 == target, right = mid - 1 = 3 - 1 = 2, ans = mid = 3
-     *
-     * 第一个nums[mid] >= target 的索引为 ans = 3，所以 leftIndex = 3
-     *
-     * 4、按上面的步骤，得出rightIndex = 4
+     * 需要考虑三种情况：
+     * 1、target在数组范围的右边或者左边，target在左边。例如数组[3,4,5]，target=2，此时返回[-1,-1]
+     * 2、target在数组范围中，且数组不存在target。例如数组[3,6,7]，target=5，此时返回[-1,-1]
+     * 3、target在数组范围中，且数组中存在target，例如数组[3,6,7]，target=6，此时返回[1,1]
      *
      * T:O(logn)
      * S:O(1)
@@ -72,28 +37,53 @@ public class SearchRange {
             return new int[] {-1, -1};
         }
 
-        int leftIndex = binarySearch(nums, target, true);
-        int rightIndex = binarySearch(nums, target, false) - 1;
-        if (leftIndex <= rightIndex && rightIndex < nums.length
-                && nums[leftIndex] == target && nums[rightIndex] == target) {
-            return new int[] {leftIndex, rightIndex};
+        int leftBorder = getLeftBorder(nums, target);
+        int rightBorder = getRightBorder(nums, target);
+
+        // 情况1
+        if (leftBorder == -2 || rightBorder == -2) {
+            return new int[] {-1, -1};
         }
+
+        // 情况3
+        if (rightBorder - leftBorder > 1) {
+            return new int[] {leftBorder + 1, rightBorder - 1};
+        }
+
+        // 情况2
         return new int[] {-1, -1};
     }
 
-    private int binarySearch(int[] nums, int target, boolean lower) {
+    private int getLeftBorder(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        int ans = nums.length;
+        int leftBorder = -2;
         while (left <= right) {
-            int mid = left + ((right - left) / 2);
-            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) { // 寻找左边界，那么右区间要往左边收缩范围
                 right = mid - 1;
-                ans = mid;
+                leftBorder = right;
             } else {
                 left = mid + 1;
             }
         }
-        return ans;
+        return leftBorder;
+    }
+
+    private int getRightBorder(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int rightBorder = -2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                // 找右边界，左区间需要往右边收缩范围
+                left = mid + 1;
+                rightBorder = left;
+            }
+        }
+        return rightBorder;
     }
 }
