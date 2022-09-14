a6de375be101e1be33cdc3e330391c9355370a45
weiwenhuan <wenhuan.wei@ximalaya.com>
2022-09-14 18:17:24 +0800

    kmp：重复的子字符串算法添加题解和算法描述

--- src/string/RepeatedSubstringPattern.java
+++ src/string/RepeatedSubstringPattern.java
@@ -28,16 +28,44 @@ public class RepeatedSubstringPattern {
      * 该题目同样是经典的kmp题型，从一个字符串中找是否有子串符合
      *
      * 获取到前缀表 next 后，判断是否有重复可构成字符串的子串，需要满足条件（假设字符串长度为 n)：
-     * 1、前缀表 next 最后一个元素 != 0
-     * 说明字符串有最长相同的前后缀（就是字符串里的前缀子串和后缀子串相同的最长长度），最长相等前后缀长度 next[n - 1]
+     * 1、前缀表 next 最后一个元素 != -1
+     * 说明字符串有最长相同的前后缀（就是字符串里的前缀子串和后缀子串相同的最长长度），最长相等前后缀长度 next[n - 1] + 1
      *
-     * 2、n % (n - next[n - 1]) == 0，可以被整除说明该字符串有重复的子串
-     * n - next[n - 1](字符串长度 - 最长相同前后缀的长度)，相当于是一个周期的长度，如果这个周期可以被整除就是这个周期的循环
+     * 2、n % (n - (next[n - 1] + 1) == 0，可以被整除说明该字符串有重复的子串
+     * n - (next[n - 1] + 1)【字符串长度 - 最长相同前后缀的长度】，相当于是一个周期的长度，如果这个周期可以被整除就是这个周期的循环
+     *
+     * 字符串：a  s  d  f  a  s  d  f  a  s  d  f
+     * next：-1 -1 -1 -1  0  1  2  3  4  5  6  7
+     *
+     * next[n - 1] = 7, next[n - 1] + 1 = 8, 8 就是此时字符串 asdfasdfasdf 的最长相同前后缀的长度
+     * n - (next[n-1] + 1)：12【字符串的长度】- 8【最长公共前后缀的长度】= 4，4 正好可以被 12（字符串的长度）整除，所以说明有重复的子字符串（asdf）
      *
      * T:O(n)
      * S:O(n)
      */
+    // 前缀表不减1
     public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        int[] next = new int[n];
+        int j = -1;
+        next[0] = j;
+        for (int i = 1; i < n; i++) {
+            while (j >= 0 && s.charAt(i) != s.charAt(j + 1)) {
+                j = next[j];
+            }
+            if (s.charAt(i) == s.charAt(j + 1)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        if (next[n - 1] != -1 && n % (n - (next[n - 1] + 1)) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    // 前缀表减1
+    public boolean repeatedSubstringPattern1(String s) {
         int n = s.length();
         int[] next = new int[n];
         for (int i = 1, j = 0; i < n; i++) {
